5c51adb5e75ca4164db6de396c754ca4a1ce3d24
shivraj110504 <tawareshivraj11@gmail.com>
2024-06-20 02:48:56 +0530

    Add First Code

---
+++ Basics/Basic.java
@@ -0,0 +1,8 @@
+package Basics;
+
+public class Basic {
+    public static void main(String[] args) {
+        System.out.println("hello World");
+        System.out.println("Hello Shivraj");
+    }
+}
