3072f45763c929636b35b0494d1401d7e888eab4
shivraj110504 <tawareshivraj11@gmail.com>
2024-06-20 02:53:29 +0530

    Add Kilometer to Miles Converter

---
+++ Basics/KmToMiles.java
@@ -0,0 +1,15 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class KmToMiles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enetr the Kilometter :");
+        int km = sc.nextInt();
+
+        float result = km * 0.621371f;
+
+        System.out.printf("%d km to miles is %f", km, result);
+    }
+}
