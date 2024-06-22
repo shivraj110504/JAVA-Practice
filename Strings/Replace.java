d34c7db4f93e300890cb214e630b22715840d1f2
shivraj110504 <tawareshivraj11@gmail.com>
2024-06-22 22:41:29 +0530

    add replace function

---
+++ Strings/Replace.java
@@ -0,0 +1,8 @@
+package Strings;
+
+public class Replace {
+    public static void main(String[] args) {
+        String letter = "Dear <name> trans a lot ";
+        System.out.println(letter.replace("<name>", "Shreyash"));
+    }
+}
