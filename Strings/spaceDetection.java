c3e8b26b7052a86238a96618601f826b9091aefe
shivraj110504 <tawareshivraj11@gmail.com>
2024-06-22 23:02:32 +0530

    add to check double and triple spaces in string

---
+++ Strings/spaceDetection.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class spaceDetection {
+    public static void main(String[] args) {
+  
+        String str = "Hello  Taware Shivaraj   How are you";
+
+        System.out.println(str.indexOf("  "));
+        System.out.println(str.indexOf("   "));
+    }
+}
