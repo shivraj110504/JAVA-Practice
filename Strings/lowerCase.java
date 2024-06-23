d777f52ee0f91198b3dae9125c8b585fea391257
shivraj110504 <tawareshivraj11@gmail.com>
2024-06-23 22:14:45 +0530

    add to cconvert string into lowercase

---
+++ Strings/lowerCase.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class lowerCase {
+    public static void main(String[] args) {
+        String string = "Hello Shivraj Taware";
+        System.out.println(string.toLowerCase());
+
+    }
+}
