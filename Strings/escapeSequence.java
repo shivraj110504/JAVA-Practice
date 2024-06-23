525375c647098c26b1eeea3ff053583f78731d34
shivraj110504 <tawareshivraj11@gmail.com>
2024-06-23 22:04:56 +0530

    Add Escape Sequence Problem Solutuin

---
+++ Strings/escapeSequence.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class escapeSequence {
+    public static void main(String[] args) {
+        String letter = "Dear Shivraj This java course is nice Thanks";
+        letter = "Dear Shivraj\n \t This java course is nice\n \"Thanks\"";
+        System.out.println(letter);
+    }
+}
