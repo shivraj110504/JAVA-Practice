23b6b0c331ddab9e57eed634928cde650d2fec35
shivraj110504 <tawareshivraj11@gmail.com>
2024-06-23 22:26:35 +0530

    Add questions on Strings

---
+++ Strings/Questios.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class Questios {
+    public static void main(String[] args) {
+        
+        1) Write a java program to convert string into lowerCase
+        2) write a program to fill the letter template which looks like below
+            letter = "Dear <name> trans a lot."
+            Replace <name> with string (some name)
+
+        3) write a program to add double and triple spaces in a String
+        4) write to format following letter using escape sequence charectors.
+            letter = "Dear Shivraj This java course is nice Thanks"
+    }
+}
